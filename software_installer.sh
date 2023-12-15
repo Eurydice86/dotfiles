5796d4be800e2f51e7a5e348a796d0b529e2c1bf
Nikos Kyriakopoulos <37345138+Eurydice86@users.noreply.github.com>
2023-12-15 20:00:19 +0200

    Update software_installer.sh

--- software_installer.sh
+++ software_installer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 pacman -S emacs exa wofi zsh ttf-iosevka-nerd --noconfirm
-
 chsh -s $(which zsh)
+reboot
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
