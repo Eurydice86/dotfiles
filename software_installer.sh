eec6ee5e00d8ecc0883774b813d6f5072bdf41f4
Nikos Kyriakopoulos <37345138+Eurydice86@users.noreply.github.com>
2023-12-15 19:22:51 +0200

    Update software_installer.sh

--- software_installer.sh
+++ software_installer.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-pacman -S emacs exa wofi alacritty zsh ttf-iosevka-nerd --noconfirm
+pacman -S emacs exa wofi zsh ttf-iosevka-nerd --noconfirm
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-rm -rf .oh-my-zsh/.git
-
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
 rm -rf .oh-my-zsh/custom/themes/powerlevel10k/.git
