ec0d99677ca90a23c6e06d9f2339384c20e64a3a
Nikos Kyriakopoulos <37345138+Eurydice86@users.noreply.github.com>
2023-12-16 23:25:47 +0200

    Update software_installer.sh

--- software_installer.sh
+++ software_installer.sh
@@ -9,4 +9,5 @@ pacman -S emacs exa wofi zsh ttf-iosevka-nerd --noconfirm
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-cp -rT dotfiles ~
+cp -rT dotfiles/.* ~
+cp -r dotfiles/wallpapers ~
