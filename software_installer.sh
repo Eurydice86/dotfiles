9199329e2a5946a100b63108844f602d067762b7
Nikos Kyriakopoulos <37345138+Eurydice86@users.noreply.github.com>
2023-12-19 00:13:18 +0200

    Update software_installer.sh

--- software_installer.sh
+++ software_installer.sh
@@ -3,8 +3,8 @@
 pacman -Suy --noconfirm
 pacman -S stow emacs exa wofi zsh ttf-iosevka-nerd --noconfirm
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/powerlevel10k
 
-stow -d dotfiles/ .dotfiles -t ~
+stow -d $HOME/dotfiles/ .dotfiles -t $HOME
 
 chsh -s $(which zsh) $USER
