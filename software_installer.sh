6e735162af9fbe8216bc797ac51ef19c6e3f2b07
Eurydice86 <kyriakopoulos.nikos@gmail.com>
2023-12-18 21:33:30 +0200

    zsh and p10k fixes?

--- software_installer.sh
+++ software_installer.sh
@@ -1,10 +1,11 @@
  #!/bin/bash
 
+pacman -Suy
 pacman -S emacs exa wofi zsh ttf-iosevka-nerd --noconfirm
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
-cd ~/dotfiles/.dotfiles
-stow .
-cd ~
+stow -d dotfiles/ .dotfiles -t ~
+
+chsh -s ($which zsh) $USER
