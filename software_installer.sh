6958e65edccddd45fcaf9de5aea5536b943399ab
Nikos Kyriakopoulos <37345138+Eurydice86@users.noreply.github.com>
2023-12-19 00:20:23 +0200

    Update software_installer.sh

--- software_installer.sh
+++ software_installer.sh
@@ -1,10 +1,12 @@
  #!/bin/bash
 
-pacman -Suy --noconfirm
-pacman -S stow emacs exa wofi zsh ttf-iosevka-nerd --noconfirm
+sudo pacman -Suy --noconfirm
+sudo pacman -S stow emacs exa wofi zsh ttf-iosevka-nerd --noconfirm
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/powerlevel10k
 
-stow -d $HOME/dotfiles/ .dotfiles -t $HOME
+stow -d dotfiles/ .dotfiles -t $HOME
 
 chsh -s $(which zsh) $USER
+
+mv dotfiles/wallpapers ~
