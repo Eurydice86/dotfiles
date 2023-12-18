b8e710788f384aed83374797924bbb781fe74234
Nikos Kyriakopoulos <37345138+Eurydice86@users.noreply.github.com>
2023-12-18 21:42:11 +0200

    Update software_installer.sh

--- software_installer.sh
+++ software_installer.sh
@@ -1,11 +1,11 @@
  #!/bin/bash
 
-pacman -Suy
-pacman -S emacs exa wofi zsh ttf-iosevka-nerd --noconfirm
+pacman -Suy --noconfirm
+pacman -S stow emacs exa wofi zsh ttf-iosevka-nerd --noconfirm
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
 stow -d dotfiles/ .dotfiles -t ~
 
-chsh -s ($which zsh) $USER
+chsh -s $(which zsh) $USER
