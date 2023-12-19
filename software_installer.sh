8e3a47872e8f50d5b7c527c3f8365952ab6f1f54
Nikos Kyriakopoulos <37345138+Eurydice86@users.noreply.github.com>
2023-12-19 10:40:34 +0200

    Update software_installer.sh

--- software_installer.sh
+++ software_installer.sh
@@ -1,14 +1,21 @@
  #!/bin/bash
 
 sudo pacman -Suy --noconfirm
-sudo pacman -S stow emacs exa wofi zsh ttf-iosevka-nerd --noconfirm
+
+sudo pacman -S --needed base-devel git --noconfirm
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+paru
+
+cd ~
+sudo pacman -S wofi exa zsh stow ttf-iosevka-nerd emacs --noconfirm
+
+chsh -s $(which zsh) $USER
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/powerlevel10k
 
 stow -d dotfiles/ .dotfiles -t $HOME
 
-chsh -s $(which zsh) $USER
-
 mv dotfiles/wallpapers ~
-
 reboot
