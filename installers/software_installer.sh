3751489e0e13a3243ef666bbce202e75a2286ccd
Eurydice86 <kyriakopoulos.nikos@gmail.com>
2024-06-30 00:26:11 +0300

    added clang

--- installers/software_installer.sh
+++ installers/software_installer.sh
@@ -13,7 +13,7 @@ cd paru
 makepkg -si
 paru
 
-sudo pacman -S wofi eza zsh atuin firefox btop stow ttf-iosevka-nerd noto-fonts-emoji pulseaudio emacs python-lsp-server python-lsp-black python-isort rust-analyzer cmake wl-clipboard thefuck --noconfirm
+sudo pacman -S wofi eza zsh atuin firefox btop stow ttf-iosevka-nerd noto-fonts-emoji pulseaudio emacs python-lsp-server python-lsp-black python-isort rust-analyzer cmake wl-clipboard clang thefuck --noconfirm
 
 paru -S sddm-sugar-candy
 
